4459c8961a62626c12aaf05a1323c0fefe33c61a
YeonginKim <fhtmah12@gmail.com>
2022-02-06 23:31:51 +0900

    created Employee domain

---
+++ ModernJava/src/main/java/com/example/modernjava/domains/Employee.java
@@ -0,0 +1,37 @@
+package com.example.modernjava.domains;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Employee {
+    Integer id;
+    String name;
+    Date hireDate;
+
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{id=%d, name='%s', hireDate=%s}".formatted(id, name, hireDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+
+        if (id != null ? !id.equals(employee.id) : employee.id != null) return false;
+        if (name != null ? !name.equals(employee.name) : employee.name != null) return false;
+        return hireDate != null ? hireDate.equals(employee.hireDate) : employee.hireDate == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (hireDate != null ? hireDate.hashCode() : 0);
+        return result;
+    }
+}
