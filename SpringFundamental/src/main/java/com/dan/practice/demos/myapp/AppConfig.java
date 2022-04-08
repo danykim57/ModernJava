dae1aa4b9bf43745eb7b2b0571d9ecd567db18ea
YeonginKim <fhtmah12@gmail.com>
2022-04-08 21:53:23 +0900

    JdbcDaoSupport demo

--- SpringFundamental/src/main/java/com/dan/practice/demos/myapp/AppConfig.java
+++ SpringFundamental/src/main/java/com/dan/practice/demos/myapp/AppConfig.java
@@ -1,9 +1,12 @@
 package com.dan.practice.demos.myapp;
 
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import javax.sql.DataSource;
 
@@ -13,11 +16,19 @@ public class AppConfig {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        //Reusage of the same connection for the ease of test. Someone should close the connection via the close() method
+//        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
+
+
+        //This is Apache tomcat dbcp(database connetion pool)
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:mydb");
+        //dataSource.setUrl("jdbc:h2:mem:mydb"); //h2 memory db
+        dataSource.setUrl("jdbc:h2:file:~/ModernJava/mydb;AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
+        //dataSource.setMaxActive(5);  //Methods from BasicDataSource
+        //dataSource.setMaxIdle(30000);
         return dataSource;
     }
 }
--- SpringFundamental/src/main/java/com/dan/practice/demos/myapp/DAO.java
+++ SpringFundamental/src/main/java/com/dan/practice/demos/myapp/DAO.java
@@ -1,15 +1,30 @@
 package com.dan.practice.demos.myapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
-public class DAO {
+public class DAO extends JdbcDaoSupport {
 
     private DataSource dataSource;
 
+    @Autowired
     public DAO(DataSource dataSource) {
-        this.dataSource = dataSource;
+        setDataSource(dataSource);
     }
+
+    @PostConstruct
+    public void doQuery() {
+        String result = getJdbcTemplate().queryForObject("select 1 from dual", String.class);
+        System.out.println("result = " + result);
+    }
+
 }
